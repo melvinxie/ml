14df6e1d25af50d8c68b7884f0c11f33c316ac0f
Mingmin Xie <melvinxie@gmail.com>
2013-12-01 16:36:25 +0900

    Multiple variables.

--- linear_regression.py
+++ linear_regression.py
@@ -1,33 +1,39 @@
-"""Gradient descent for linear regression with one variable."""
+"""Gradient descent for linear regression."""
 
+from copy import copy
+from numpy import dot
 from scipy.misc import derivative
 
-def hypothesis(t0, t1, x):
-    return t0 + t1 * x
+def h(t, x):
+    return dot(t, x)
 
-def J(t0, t1, x, y):
+def J(t, x, y):
     """Cost function."""
     m = len(x)
-    return sum((hypothesis(t0, t1, x[i]) - y[i])**2 for i in range(m)) / 2 / m
+    return sum((h(t, x[i]) - y[i])**2 for i in range(m)) / 2 / m
 
-def gradient_descent(t00, t10, x, y):
+def gradient_descent(t0, x , y):
     E = 1e-6
     m = len(x)
     a = 0.1
-    t0 = t00
-    t1 = t10
+    t = t0
     while True:
-        def J1(theta0):
-            return J(theta0, t1, x, y)
-        def J2(theta1):
-            return J(t0, theta1, x, y)
-        new_t0 = t0 - a * derivative(J1, t0, dx=E)
-        new_t1 = t1 - a * derivative(J2, t1, dx=E)
-        if abs(new_t0 - t0) < E and abs(new_t1 - t1) < E:
-          break
-        t0, t1 = new_t0, new_t1
-    return t0, t1
+        done = True
+        new_t = []
+        for i in range(len(t)):
+            def Ji(theta):
+                t_current = copy(t)
+                t_current[i] = theta
+                return J(t_current, x, y)
+            new_t.append(t[i] - a * derivative(Ji, t[i], dx=E))
+            if abs(new_t[i] - t[i]) > E:
+                done = False
+        if done:
+            break
+        t = new_t
+    return t
 
-x = [0, 1, 2, 3]
-y = [1, 3, 5, 7]
-print gradient_descent(0, 1, x, y)  # t0, t1 converge to 1, 2
+t0 = [0, 1, 2]
+x = [[1, 0, 0], [1, 2, 1], [1, 3, 2], [1, 4, 3]]
+y = [1, 6, 9, 12]  # y = 1 + 2 * x1 + x2
+print gradient_descent(t0, x, y)  # t converges to [1, 2, 1]
